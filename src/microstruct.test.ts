5f27a52a2d1d5e1e7e744864efb03902f72ba12e
iorate <682043+iorate@users.noreply.github.com>
2023-01-15 15:49:04 +0900

    test: call `expect` directly

--- src/microstruct.test.ts
+++ src/microstruct.test.ts
@@ -1,6 +1,5 @@
 import { expect, test } from '@jest/globals';
 import {
-  Struct,
   is,
   parse,
   any,
@@ -26,210 +25,210 @@ import {
 
 // ref: https://docs.superstructjs.org/api-reference/types
 
-function testValid<T, V extends T>(value: V, struct: Struct<T>): void {
-  expect(is(value, struct)).toBe(true);
-}
-
-function testInvalid<T>(value: unknown, struct: Struct<T>): void {
-  expect(is(value, struct)).toBe(false);
-}
-
 test('any structs accept any value as valid', () => {
-  testValid('valid', any());
-  testValid(42, any());
-  testValid(true, any());
-  testValid(undefined, any());
-  testValid(null, any());
-  testValid({ also: 'valid' }, any());
+  expect(is('valid', any())).toBe(true);
+  expect(is(42, any())).toBe(true);
+  expect(is(true, any())).toBe(true);
+  expect(is(undefined, any())).toBe(true);
+  expect(is(null, any())).toBe(true);
+  expect(is({ also: 'valid' }, any())).toBe(true);
 });
 
 test('array structs accept a list of values of a specific type', () => {
-  testValid([1, 2, 3], array(number()));
-  testValid([{ id: 1 }], array(object({ id: number() })));
-  testInvalid('invalid', array(number()));
-  testInvalid({ 0: 1, 1: 2, 2: 3 }, array(number()));
-  testInvalid(['1', '2', '3'], array(number()));
-  testInvalid([1, 2, '3'], array(number()));
+  expect(is([1, 2, 3], array(number()))).toBe(true);
+  expect(is([{ id: 1 }], array(object({ id: number() })))).toBe(true);
+  expect(is('invalid', array(number()))).toBe(false);
+  expect(is({ 0: 1, 1: 2, 2: 3 }, array(number()))).toBe(false);
+  expect(is(['1', '2', '3'], array(number()))).toBe(false);
+  expect(is([1, 2, '3'], array(number()))).toBe(false);
 });
 
 test('boolean structs accept the boolean values true and false', () => {
-  testValid(true, boolean());
-  testValid(false, boolean());
-  testInvalid('true', boolean());
-  testInvalid(0, boolean());
-  testInvalid({ also: 'invalid' }, boolean());
-  testInvalid(new Boolean(true), boolean());
+  expect(is(true, boolean())).toBe(true);
+  expect(is(false, boolean())).toBe(true);
+  expect(is('true', boolean())).toBe(false);
+  expect(is(0, boolean())).toBe(false);
+  expect(is({ also: 'invalid' }, boolean())).toBe(false);
+  expect(is(new Boolean(true), boolean())).toBe(false);
 });
 
 test('enums structs validate that a value is one of a specific set of literals values', () => {
-  testValid('Jane', enums(['Jane', 'John', 'Jack', 'Jill']));
-  testValid(42, enums([23, 42]));
-  testInvalid('Joe', enums(['Jane', 'John', 'Jack', 'Jill']));
-  testInvalid('23', enums([23, 42]));
+  expect(is('Jane', enums(['Jane', 'John', 'Jack', 'Jill']))).toBe(true);
+  expect(is(42, enums([23, 42]))).toBe(true);
+  expect(is('Joe', enums(['Jane', 'John', 'Jack', 'Jill']))).toBe(false);
+  expect(is('23', enums([23, 42]))).toBe(false);
 });
 
 test('integer structs validate that a value is an integer', () => {
-  testValid(-7, integer());
-  testValid(0, integer());
-  testValid(42, integer());
-  testInvalid('42', integer());
-  testInvalid(3.14, integer());
-  testInvalid(false, integer());
-  testInvalid(new Number(42), integer());
+  expect(is(-7, integer())).toBe(true);
+  expect(is(0, integer())).toBe(true);
+  expect(is(42, integer())).toBe(true);
+  expect(is('42', integer())).toBe(false);
+  expect(is(3.14, integer())).toBe(false);
+  expect(is(false, integer())).toBe(false);
+  expect(is(new Number(42), integer())).toBe(false);
 });
 
 test('intersection structs validate that a value matches all of many structs. It takes existing struct objects as arguments', () => {
-  testValid(
-    { a: 1, b: 2, c: 3 },
-    intersection([type({ a: number(), b: number() }), type({ a: number(), c: number() })]),
-  );
-  testValid({ a: 1, b: 2, c: 3 }, intersection([]));
-  testInvalid(
-    { a: 1, b: 2 },
-    intersection([type({ a: number(), b: number() }), type({ a: number(), c: number() })]),
-  );
+  expect(
+    is(
+      { a: 1, b: 2, c: 3 },
+      intersection([type({ a: number(), b: number() }), type({ a: number(), c: number() })]),
+    ),
+  ).toBe(true);
+  expect(is({ a: 1, b: 2, c: 3 }, intersection([]))).toBe(true);
+  expect(
+    is(
+      { a: 1, b: 2 },
+      intersection([type({ a: number(), b: number() }), type({ a: number(), c: number() })]),
+    ),
+  ).toBe(false);
 });
 
 test('literal structs enforce that a value matches an exact value using the === operator', () => {
-  testValid(42, literal(42));
-  testValid('valid', literal('valid'));
-  testValid(true, literal(true));
-  testInvalid(23, literal(42));
-  testInvalid('42', literal(42));
-  testInvalid('invalid', literal('valid'));
-  testInvalid(1, literal(true));
+  expect(is(42, literal(42))).toBe(true);
+  expect(is('valid', literal('valid'))).toBe(true);
+  expect(is(true, literal(true))).toBe(true);
+  expect(is(23, literal(42))).toBe(false);
+  expect(is('42', literal(42))).toBe(false);
+  expect(is('invalid', literal('valid'))).toBe(false);
+  expect(is(1, literal(true))).toBe(false);
 });
 
 test('never structs will fail validation for every value', () => {
-  testInvalid(42, never());
-  testInvalid(null, never());
+  expect(is(42, never())).toBe(false);
+  expect(is(null, never())).toBe(false);
 });
 
 test('nullable structs validate that a value matches a specific struct, or that it is null', () => {
-  testValid('a string of text', nullable(string()));
-  testValid(null, nullable(string()));
-  testValid({ a: null }, object({ a: nullable(string()) }));
-  testInvalid(23, nullable(string()));
-  testInvalid(undefined, nullable(string()));
-  testInvalid({}, object({ a: nullable(string()) }));
+  expect(is('a string of text', nullable(string()))).toBe(true);
+  expect(is(null, nullable(string()))).toBe(true);
+  expect(is({ a: null }, object({ a: nullable(string()) }))).toBe(true);
+  expect(is(23, nullable(string()))).toBe(false);
+  expect(is(undefined, nullable(string()))).toBe(false);
+  expect(is({}, object({ a: nullable(string()) }))).toBe(false);
 });
 
 test('object structs validate that a value is an object and that each of its properties match a specific type as well', () => {
-  testValid({ id: 1, name: 'Jane Smith' }, object({ id: number(), name: string() }));
-  testValid({ 0: 1, 1: 2, 2: 3 }, object({ 0: number(), 1: number(), 2: number() }));
-  testValid([1, 2, 3], object({ length: number(), 0: number(), 1: number(), 2: number() }));
-  testInvalid({ id: 1, name: 'Jane Smith' }, object({ id: number() }));
-  testInvalid('id', object({ id: number(), name: string() }));
-  testInvalid(1, object({ id: number(), name: string() }));
-  testInvalid({ id: 1 }, object({ id: number(), name: string() }));
-  testInvalid({ id: 1, name: false }, object({ id: number(), name: string() }));
+  expect(is({ id: 1, name: 'Jane Smith' }, object({ id: number(), name: string() }))).toBe(true);
+  expect(is({ 0: 1, 1: 2, 2: 3 }, object({ 0: number(), 1: number(), 2: number() }))).toBe(true);
+  expect(is([1, 2, 3], object({ length: number(), 0: number(), 1: number(), 2: number() }))).toBe(
+    true,
+  );
+  expect(is({ id: 1, name: 'Jane Smith' }, object({ id: number() }))).toBe(false);
+  expect(is('id', object({ id: number(), name: string() }))).toBe(false);
+  expect(is(1, object({ id: number(), name: string() }))).toBe(false);
+  expect(is({ id: 1 }, object({ id: number(), name: string() }))).toBe(false);
+  expect(is({ id: 1, name: false }, object({ id: number(), name: string() }))).toBe(false);
 
   // optional
-  testValid({ a: 23, b: 42 }, object({ a: optional(number()), b: optional(number()) }));
-  testValid({ a: 23, b: 42 }, object({ a: number(), b: optional(number()) }));
-  testValid(
-    { a: 23, b: 42 },
-    object({ a: number(), b: optional(number()), c: optional(number()) }),
+  expect(is({ a: 23, b: 42 }, object({ a: optional(number()), b: optional(number()) }))).toBe(true);
+  expect(is({ a: 23, b: 42 }, object({ a: number(), b: optional(number()) }))).toBe(true);
+  expect(
+    is({ a: 23, b: 42 }, object({ a: number(), b: optional(number()), c: optional(number()) })),
+  ).toBe(true);
+  expect(is({ a: 23, b: 42 }, object({ a: number(), b: number(), c: optional(number()) }))).toBe(
+    true,
   );
-  testValid({ a: 23, b: 42 }, object({ a: number(), b: number(), c: optional(number()) }));
-  testValid({}, object({ a: optional(number()) }));
-  testInvalid({ a: 23, b: 42 }, object({ a: number(), b: optional(string()) }));
-  testInvalid({ a: 23, b: 42 }, object({ a: number(), c: optional(number()) }));
-  testInvalid({ a: 23 }, object({ b: optional(number()), c: optional(number()) }));
+  expect(is({}, object({ a: optional(number()) }))).toBe(true);
+  expect(is({ a: 23, b: 42 }, object({ a: number(), b: optional(string()) }))).toBe(false);
+  expect(is({ a: 23, b: 42 }, object({ a: number(), c: optional(number()) }))).toBe(false);
+  expect(is({ a: 23 }, object({ b: optional(number()), c: optional(number()) }))).toBe(false);
 });
 
 test('optional structs validate that a value matches a specific struct, or that it is undefined', () => {
-  testValid('a string of text', optional(string()));
-  testValid(undefined, optional(string()));
-  testValid({ a: 23, b: 42 }, object({ a: number(), b: optional(number()) }));
-  testValid({ a: 23 }, object({ a: number(), b: optional(number()) }));
-  testInvalid(23, optional(string()));
-  testInvalid(null, optional(string()));
-  testInvalid({ a: 23, b: '42' }, object({ a: number(), b: optional(number()) }));
-  testInvalid({ a: 23, b: null }, object({ a: number(), b: optional(number()) }));
+  expect(is('a string of text', optional(string()))).toBe(true);
+  expect(is(undefined, optional(string()))).toBe(true);
+  expect(is({ a: 23, b: 42 }, object({ a: number(), b: optional(number()) }))).toBe(true);
+  expect(is({ a: 23 }, object({ a: number(), b: optional(number()) }))).toBe(true);
+  expect(is(23, optional(string()))).toBe(false);
+  expect(is(null, optional(string()))).toBe(false);
+  expect(is({ a: 23, b: '42' }, object({ a: number(), b: optional(number()) }))).toBe(false);
+  expect(is({ a: 23, b: null }, object({ a: number(), b: optional(number()) }))).toBe(false);
 });
 
 test('record structs validate an object with specific types for its keys and values', () => {
-  testValid({ a: 1, b: 2 }, record(string(), number()));
-  testInvalid('a', record(string(), number()));
-  testInvalid(1, record(string(), number()));
-  testInvalid({ a: 1, b: '2' }, record(string(), number()));
+  expect(is({ a: 1, b: 2 }, record(string(), number()))).toBe(true);
+  expect(is('a', record(string(), number()))).toBe(false);
+  expect(is(1, record(string(), number()))).toBe(false);
+  expect(is({ a: 1, b: '2' }, record(string(), number()))).toBe(false);
 });
 
 test('string structs validate that a value is a string', () => {
-  testValid('a string of text', string());
-  testValid('42', string());
-  testInvalid(42, string());
-  testInvalid(['a string of text'], string());
+  expect(is('a string of text', string())).toBe(true);
+  expect(is('42', string())).toBe(true);
+  expect(is(42, string())).toBe(false);
+  expect(is(['a string of text'], string())).toBe(false);
 });
 
 test('tuple structs validate that a value is an array of a specific length with values each of a specific type', () => {
-  testValid(['a', 1, true], tuple([string(), number(), boolean()]));
-  testValid([], tuple([]));
-  testInvalid('a', tuple([string(), number(), boolean()]));
-  testInvalid(1, tuple([string(), number(), boolean()]));
-  testInvalid(['a', 1], tuple([string(), number(), boolean()]));
-  testInvalid(['a', 1, true, []], tuple([string(), number(), boolean()]));
-  testInvalid(['a'], tuple([]));
+  expect(is(['a', 1, true], tuple([string(), number(), boolean()]))).toBe(true);
+  expect(is([], tuple([]))).toBe(true);
+  expect(is('a', tuple([string(), number(), boolean()]))).toBe(false);
+  expect(is(1, tuple([string(), number(), boolean()]))).toBe(false);
+  expect(is(['a', 1], tuple([string(), number(), boolean()]))).toBe(false);
+  expect(is(['a', 1, true, []], tuple([string(), number(), boolean()]))).toBe(false);
+  expect(is(['a'], tuple([]))).toBe(false);
 
   // optional
-  testValid(['a', 1], tuple([optional(string()), optional(number())]));
-  testValid(['a', 1], tuple([string(), optional(number())]));
-  testValid(['a', 1], tuple([string(), number(), optional(boolean())]));
-  testValid(['a', 1], tuple([string(), optional(number()), optional(boolean())]));
-  testValid([], tuple([optional(string())]));
-  testInvalid(['a', 1], tuple([optional(number()), optional(number())]));
-  testInvalid(['a', 1], tuple([string(), optional(string())]));
+  expect(is(['a', 1], tuple([optional(string()), optional(number())]))).toBe(true);
+  expect(is(['a', 1], tuple([string(), optional(number())]))).toBe(true);
+  expect(is(['a', 1], tuple([string(), number(), optional(boolean())]))).toBe(true);
+  expect(is(['a', 1], tuple([string(), optional(number()), optional(boolean())]))).toBe(true);
+  expect(is([], tuple([optional(string())]))).toBe(true);
+  expect(is(['a', 1], tuple([optional(number()), optional(number())]))).toBe(false);
+  expect(is(['a', 1], tuple([string(), optional(string())]))).toBe(false);
 });
 
 test('type structs validate that a value has a set of properties on it, but it does not assert anything about unspecified properties', () => {
-  testValid({ id: 1, name: 'Jane Smith' }, type({ id: number(), name: string() }));
-  testValid({ id: 1, name: 'Jane Smith' }, type({ id: number() }));
-  testValid({ a: 23, b: 42 }, type({ a: number(), b: optional(number()) }));
-  testValid({ a: 23 }, type({ a: number(), b: optional(number()) }));
-  testInvalid({ id: 1 }, type({ id: number(), name: string() }));
-  testInvalid({ id: 1, name: false }, type({ id: number(), name: string() }));
-  testInvalid({ a: 23, b: '42' }, type({ a: number(), b: optional(number()) }));
+  expect(is({ id: 1, name: 'Jane Smith' }, type({ id: number(), name: string() }))).toBe(true);
+  expect(is({ id: 1, name: 'Jane Smith' }, type({ id: number() }))).toBe(true);
+  expect(is({ a: 23, b: 42 }, type({ a: number(), b: optional(number()) }))).toBe(true);
+  expect(is({ a: 23 }, type({ a: number(), b: optional(number()) }))).toBe(true);
+  expect(is({ id: 1 }, type({ id: number(), name: string() }))).toBe(false);
+  expect(is({ id: 1, name: false }, type({ id: number(), name: string() }))).toBe(false);
+  expect(is({ a: 23, b: '42' }, type({ a: number(), b: optional(number()) }))).toBe(false);
 });
 
 test('union structs validate that a value matches at least one of many types', () => {
-  testValid('a string', union([string(), number()]));
-  testValid(42, union([string(), number()]));
-  testInvalid(true, union([string(), number()]));
-  testInvalid(['a string', 42], union([string(), number()]));
-  intersection([]);
+  expect(is('a string', union([string(), number()]))).toBe(true);
+  expect(is(42, union([string(), number()]))).toBe(true);
+  expect(is(true, union([string(), number()]))).toBe(false);
+  expect(is(['a string', 42], union([string(), number()]))).toBe(false);
 });
 
 test('unknown structs accept unknown value as valid without loosening its type to any', () => {
-  testValid('valid', unknown());
-  testValid(42, unknown());
-  testValid(true, unknown());
-  testValid(undefined, unknown());
-  testValid(null, unknown());
-  testValid({ also: 'valid' }, unknown());
+  expect(is('valid', unknown())).toBe(true);
+  expect(is(42, unknown())).toBe(true);
+  expect(is(true, unknown())).toBe(true);
+  expect(is(undefined, unknown())).toBe(true);
+  expect(is(null, unknown())).toBe(true);
+  expect(is({ also: 'valid' }, unknown())).toBe(true);
 });
 
 test('define structs take validator functions that return either true/false', () => {
-  testValid(
-    42,
-    define<number>(n => n === 42),
-  );
-  testInvalid(
-    54,
-    define<number>(n => n === 42),
-  );
+  expect(
+    is(
+      42,
+      define<number>(n => n === 42),
+    ),
+  ).toBe(true);
+  expect(
+    is(
+      54,
+      define<number>(n => n === 42),
+    ),
+  ).toBe(false);
 });
 
-function testValidJSON<T>(json: string, s: Struct<T>): void {
-  expect(parse(json, s)).toEqual(JSON.parse(json));
-}
-
-function testInvalidJSON<T>(json: string, s: Struct<T>): void {
-  expect(parse(json, s)).toBe(undefined);
-}
-
 test('parse() returns a typed value parsed from a JSON string if it is valid', () => {
-  testValidJSON('{"result":true, "count":42}', object({ result: boolean(), count: number() }));
-  testInvalidJSON('{"result":true, "count":42}', object({ result: boolean(), count: string() }));
-  testInvalidJSON('{"result":true, "count":42', object({ result: boolean(), count: number() }));
+  expect(
+    parse('{"result":true, "count":42}', object({ result: boolean(), count: number() })),
+  ).toEqual({ result: true, count: 42 });
+  expect(parse('{"result":true, "count":42}', object({ result: boolean(), count: string() }))).toBe(
+    undefined,
+  );
+  expect(parse('{"result":true, "count":42', object({ result: boolean(), count: number() }))).toBe(
+    undefined,
+  );
 });
